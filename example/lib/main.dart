c2af27ef1befca774382f126f86d73f8f8c70410
manhal_akram_khwashki <manhal.akram.khwashki@gmail.com>
2024-11-04 23:45:34 +0300

    init

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hashtag_text/hashtag_text.dart';
+import 'package:hashtagable_text/hashtag_text.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       ),
       home:  Scaffold(body: SafeArea(child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: HashTagText(text: "This is a #text with some #hashtags for #testing.",onHashTagTap: (hastTag){
+        child: HashTagText(text: "This is a #text with a some #hashtags for #testing.",onHashTagTap: (hastTag){
           debugPrint(hastTag);
         },),
       ))),
